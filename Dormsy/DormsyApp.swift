0496d69e1f0a3ce25b7a5d81c34d41a25663b886
vineetshub <vms118@scarletmail.rutgers.edu>
2024-11-04 18:07:45 -0500

    Initial Commit

---
+++ Dormsy/DormsyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DormsyApp.swift
+//  Dormsy
+//
+//  Created by Vineet Shah on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct DormsyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
